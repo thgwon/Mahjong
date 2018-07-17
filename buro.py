ec9c7c99995eb9f011edef3c6a4d0622dc964545
thgwon <thgwon@postech.ac.kr>
2018-07-17 18:03:57 +0900

    buro.py 수정

--- buro.py
+++ buro.py
@@ -1,38 +1,31 @@
 class Buro:
-    def __init__(self, tile, base_fu):
+    def __init__(self, tile):
         self._tile = None
-        self._base_fu = None
-
-    @property
-    def fu(self):
-        return self._base_fu * (2 if self._tile.is_terminal_and_honor else 1)
-
 
 class Chi(Buro):
     # type : 0 -> a-2, a-1, a
     #        1 -> a-1, a, a+1
     #        2 -> a, a+1, a+2
     def __init__(self, tile, type):
-        super.__init__(tile, 0)
+        super.__init__(tile)
         self._type = None
-        self._fu = 0
 
 class Pon(Buro):
     def __init__(self, tile, where):
-        super.__init__(tile, 2)
+        super.__init__(tile)
         self._where = where
 
 class BigMeldedKan(Buro):
     def __init__(self, tile, where):
-        super.__init__(tile, 8)
+        super.__init__(tile)
         self._where = where
 
 class SmallMeldedKan(Buro):
     def __init__(self, tile, where):
-        super.__init__(tile, 8)
+        super.__init__(tile)
         self._where = where
 
 class ConcealedKan(Buro):
     def __init__(self, tile):
-        super.__init__(tile, 16)
+        super.__init__(tile)
 
